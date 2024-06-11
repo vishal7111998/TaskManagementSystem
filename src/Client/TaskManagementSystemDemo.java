43289c7a46d562a1ac8d4cb1bee2eb2bab94377b
vishal7111998 <vishal7111998@gmail.com>
2024-06-12 04:32:06 +0530

    All features added.

--- src/Client/TaskManagementSystemDemo.java
+++ src/Client/TaskManagementSystemDemo.java
@@ -1,5 +1,76 @@
 package Client;
 
+import java.util.List;
+
+import Model.Task;
+import Model.User;
+import SystemManager.TaskManager;
+
 public class TaskManagementSystemDemo {
 
+	public static void main(String[] args) {
+		TaskManager taskManager = TaskManager.getTaskManagerInstance();
+
+		Task t1 = new Task("1", "Login task", "Develope a login page");
+		Task t2 = new Task("2", "SignUp task", "Develope a signup page");
+		Task t3 = new Task("3", "Ui page", "Develope a UI page");
+		Task t4 = new Task("4", "Fix bug", "Fix the bug in feature branch");
+
+		taskManager.createTask(t1);
+		taskManager.createTask(t2);
+
+		User user1 = new User("U1", "abc", "abc@gmail.com");
+		User user2 = new User("U2", "def", "def@gmail.com");
+
+		taskManager.createTask(t3);
+		taskManager.createTask(t4);
+
+		List<Task> taskList = taskManager.getAllTask();
+		for (Task task : taskList) {
+			System.out.println(task.getTitle());
+		}
+
+		taskManager.assignTaskToUser(user1, t1);
+		taskManager.assignTaskToUser(user1, t2);
+		taskManager.assignTaskToUser(user1, t4);
+		taskManager.assignTaskToUser(user2, t3);
+
+		/*
+		 * List<Task> userTasks = taskManager.getUserTaskList(user2); for(Task task :
+		 * userTasks) { System.out.println(task.getAssignedUser().getName() +
+		 * " is assigned " + task.getTitle()); }
+		 */
+
+		taskManager.unassignTaskFromUser(user1, t4);
+		taskManager.assignTaskToUser(user2, t4);
+
+		/*
+		 * List<Task> userTasks = taskManager.getUserTaskList(user2); for(Task task :
+		 * userTasks) { System.out.println(task.getAssignedUser().getName() +
+		 * " is assigned " + task.getTitle()); }
+		 * 
+		 * 
+		 * userTasks = taskManager.getUserTaskList(user1); for(Task task : userTasks) {
+		 * System.out.println(task.getAssignedUser().getName() + " is assigned " +
+		 * task.getTitle()); }
+		 */
+
+		
+		 Task updatedT1Task = new Task(t1.getId(), t1.getTitle(), t1.getDescription());
+		 updatedT1Task.setAssignedUser(user2);
+		 
+		 List<Task> userTasks = taskManager.getUserTaskList(user1);
+			for (Task task : userTasks) {
+				System.out.println(task.getAssignedUser().getName() + " is assigned " + task.getTitle());
+			}
+		 
+		 taskManager.updateTask(updatedT1Task);
+		 
+		 System.out.println("After updating t1 task");
+		userTasks = taskManager.getUserTaskList(user1);
+		for (Task task : userTasks) {
+			System.out.println(task.getAssignedUser().getName() + " is assigned " + task.getTitle());
+		}
+
+	}
 }
--- src/SystemManager/TaskManager.java
+++ src/SystemManager/TaskManager.java
@@ -1,5 +1,150 @@
 package SystemManager;
 
-public class TaskManager {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+import Exception.TaskNotFoundException;
+import Exception.UserTaskNotFoundException;
+import Model.PriorityLevel;
+import Model.Task;
+import Model.TaskStatus;
+import Model.User;
+
+public class TaskManager {
+	private static TaskManager taskManager = null;
+	Map<String, Task> tasks;
+	Map<String, List<Task>> userTasks;
+	
+	private TaskManager() {
+		tasks = new ConcurrentHashMap<>();
+		userTasks = new ConcurrentHashMap<>();
+	}
+	
+	public static TaskManager getTaskManagerInstance() {
+		if(taskManager == null) {
+			synchronized(TaskManager.class) {
+				if(taskManager == null) {
+					taskManager = new TaskManager();
+				}
+			}
+		}
+		return taskManager;
+	}
+	
+	public void createTask(Task task) {
+		tasks.put(task.getId(), task);
+	}
+	
+	public void updateTask(Task updatedTask) {
+		Task existingTask = tasks.get(updatedTask.getId());
+		if(existingTask != null) {
+			synchronized(existingTask) {
+				existingTask.setDescription(updatedTask.getDescription());
+				existingTask.setDueDate(updatedTask.getDueDate());
+				existingTask.setPriorityLevel(updatedTask.getPriorityLevel());
+				existingTask.setTaskStatus(updatedTask.getTaskStatus());
+				existingTask.setTaskType(updatedTask.getTaskType());
+				existingTask.setTitle(updatedTask.getTitle());
+				
+				User prevoiusUser = existingTask.getAssignedUser();
+				User newUser = updatedTask.getAssignedUser();
+				if(prevoiusUser == null) {
+					assignTaskToUser(newUser, existingTask);
+				}
+				else if(prevoiusUser != newUser) {
+					unassignTaskFromUser(prevoiusUser, existingTask);
+					assignTaskToUser(newUser, existingTask);	
+				}
+			}
+		}
+	}
+	
+	public void assignTaskToUser(User user, Task task) {
+		if(user != null) {
+			List<Task> taskList = userTasks.get(user.getId());
+			if(taskList == null) {
+				taskList = new ArrayList<>();
+				taskList.add(task);
+				
+				userTasks.put(user.getId(), taskList);
+			}
+			else {
+				taskList.add(task);
+			}
+			task.setAssignedUser(user);
+			tasks.put(task.getId(), task);
+		}
+	}
+	
+	public void unassignTaskFromUser(User user, Task task) {
+		List<Task> taskList = userTasks.get(user.getId());
+		if(taskList.contains(task)) {
+			taskList.remove(task);
+		}
+		userTasks.put(user.getId(), taskList);
+	}
+	
+	public void deleteTask(Task task) {
+		if(tasks.get(task.getId()) == null) {
+			throw new TaskNotFoundException();
+		}
+		
+		tasks.remove(task.getId());
+		if(task.getAssignedUser() != null) {
+			unassignTaskFromUser(task.getAssignedUser(), task);
+		}
+	}
+	
+	public Task searchTask(String id) {
+		if(tasks.get(id) == null) {
+			throw new TaskNotFoundException();
+		}
+		
+		return tasks.get(id);
+	}
+	
+	public List<Task> searchTaskByKeyword(String keyWord) {
+		List<Task> list = new ArrayList<>();
+		for(Task task : tasks.values()) {
+			if(task.getTitle().contains(keyWord) || task.getDescription().contains(keyWord)) {
+				list.add(task);
+			}
+		}
+		
+		return list;
+	}
+	
+	
+	public List<Task> filterTask(TaskStatus taskStatus, Date startDate, Date dueDate, PriorityLevel priorityLevel){
+		List<Task> list = new ArrayList<>();
+		for(Task task : tasks.values()){
+			if(task.getTaskStatus().equals(TaskStatus.IN_PROGRESS) && 
+					(task.getDueDate().compareTo(startDate) >= 0) &&
+					(task.getDueDate().compareTo(dueDate) <= 0) &&
+					task.getPriorityLevel().equals(PriorityLevel.HIGH)){
+							list.add(task);
+					}
+		}
+		return list;
+	}
+	
+	public List<Task> getUserTaskList(User user){
+		if(userTasks.get(user.getId()) == null) {
+			throw new UserTaskNotFoundException();
+		}
+		
+		return userTasks.get(user.getId());
+	}
+	
+	public List<Task> getAllTask(){
+		List<Task> list = new ArrayList<>();
+		for(Task task : tasks.values()) {
+			list.add(task);
+		}
+		
+		return list;
+	}
 }
